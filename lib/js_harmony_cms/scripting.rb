8ed9b4baa5c7ec3130224d168a435fe1b0667ae2
Justin Love <jl@apharmony.com>
2021-10-20 14:03:59 -0500

    port scripting module

---
+++ lib/js_harmony_cms/scripting.rb
@@ -0,0 +1,15 @@
+require 'erb'
+
+class JsHarmonyCms; end
+module JsHarmonyCms::Scripting
+  def self.remote_template_integration(cms_clientjs_editor_launcher_path, access_key)
+    %Q(<script type="text/javascript" class="removeOnPublish" src="#{cms_clientjs_editor_launcher_path}"></script>
+    <script type="text/javascript" class="removeOnPublish">
+    jsHarmonyCmsEditor({"access_keys":["#{access_key}"]});
+    </script>)
+  end
+
+  def self.editor_script(cms_server_url)
+    %Q(<script type="text/javascript" src="#{ERB::Util.html_escape(cms_server_url + 'js/jsHarmonyCMS.js')}"></script>)
+  end
+end
---
+++ test/scripting_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+require 'js_harmony_cms/scripting'
+
+class ScriptingTest < Minitest::Test
+  Scripting = JsHarmonyCms::Scripting
+
+  def test_remote_template_runs
+    script = Scripting.remote_template_integration('launcher', 'token')
+    assert_match /script/, script
+    assert_match /launcher/, script
+    assert_match /token/, script
+  end
+
+  def test_editor_script_runs
+    script = Scripting.editor_script('server')
+    assert_match /script/, script
+    assert_match /server/, script
+  end
+end
