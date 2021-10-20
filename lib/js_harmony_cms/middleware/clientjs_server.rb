abb6ccb3267f45375b11dc80ebffda16dd1426cd
Justin Love <jl@apharmony.com>
2021-10-20 14:20:15 -0500

    port page class, reformat all in standard indented module style

--- lib/js_harmony_cms/middleware/clientjs_server.rb
+++ lib/js_harmony_cms/middleware/clientjs_server.rb
@@ -1,36 +1,38 @@
 require 'rack/request'
 
-class JsHarmonyCms; end
-module JsHarmonyCms::Middleware; end
-class JsHarmonyCms::Middleware::ClientjsServer
-  def initialize(app, clientjs_url)
-    @app = app
-    @clientjs_url = clientjs_url
-  end
-
-  def call(env)
-    path = Rack::Request.new(env).path
-    return [200, {'Content-Type' => 'application/javascript'}, [load_file]] if path == @clientjs_url
-
-    @app.call(env)
-  end
-
-  def load_file
-    File.read(File.dirname(__FILE__)+'/../../../clientjs/jsHarmonyCmsEditor.min.js')
-  rescue => error
-    report_error error
-    return ""
-  end
-
-  def report_error(error)
-    if Object.const_defined?('Rails') && Rails.method_defined?('env')
-      if Rails.env.test?
-        p error
-      else
-        Rails.logger.error error
+class JsHarmonyCms
+  module Middleware
+    class ClientjsServer
+      def initialize(app, clientjs_url)
+        @app = app
+        @clientjs_url = clientjs_url
       end
-    else
-      p error
+
+      def call(env)
+        path = Rack::Request.new(env).path
+        return [200, {'Content-Type' => 'application/javascript'}, [load_file]] if path == @clientjs_url
+
+        @app.call(env)
+      end
+
+      def load_file
+        File.read(File.dirname(__FILE__)+'/../../../clientjs/jsHarmonyCmsEditor.min.js')
+      rescue => error
+        report_error error
+        return ""
+      end
+
+      def report_error(error)
+        if Object.const_defined?('Rails') && Rails.method_defined?('env')
+          if Rails.env.test?
+            p error
+          else
+            Rails.logger.error error
+          end
+        else
+          p error
+        end
+      end
     end
   end
 end
